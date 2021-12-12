51c6937c6c3b8c51ec2bf4d5769a8f7842dcd2c3
Prakhar Sahay <prakhar.studios@gmail.com>
2021-12-12 15:41:39 -0500

    [Lint] Spaces -> Tabs

--- js/test/array-monkey-patch.test.js
+++ js/test/array-monkey-patch.test.js
@@ -2,50 +2,50 @@ const assert = require("assert");
 require("../array-monkey-patch.js");
 
 describe("Array Monkey Patch module", () => {
-    describe("#groupBy()", () => {
-        it("groups by state", () => {
-            const input = [{a: "MA", b: 10}, {a: "MA", b: 50}, {a: "NY", b: 20}];
-            const actual = input.groupBy(row => row.a);
+	describe("#groupBy()", () => {
+		it("groups by state", () => {
+			const input = [{a: "MA", b: 10}, {a: "MA", b: 50}, {a: "NY", b: 20}];
+			const actual = input.groupBy(row => row.a);
 
-            const expected =  [
-                [ "MA", [
-                    {a: "MA", b: 10},
-                    {a: "MA", b: 50}
-                ]],
-                [ "NY", [
-                    {a: "NY", b: 20}
-                ]]
-            ];
+			const expected =  [
+				[ "MA", [
+					{a: "MA", b: 10},
+					{a: "MA", b: 50}
+				]],
+				[ "NY", [
+					{a: "NY", b: 20}
+				]]
+			];
 
-            assert.deepEqual(actual, expected);
-        });
-    });
+			assert.deepEqual(actual, expected);
+		});
+	});
 
-    describe("#innerJoin()", () => {
-        it("joins by the given left and right keys", () => {
-            const actual = [{a: "MA", b: 10}, {a: "MA", b: 50}, {a: "NY", b: 20}]
-                .innerJoin([{d: "MA", c: "Boston"}, {d: "NY", c: "Albany"}], ["a"], ["d"]);
+	describe("#innerJoin()", () => {
+		it("joins by the given left and right keys", () => {
+			const actual = [{a: "MA", b: 10}, {a: "MA", b: 50}, {a: "NY", b: 20}]
+				.innerJoin([{d: "MA", c: "Boston"}, {d: "NY", c: "Albany"}], ["a"], ["d"]);
 
-            const expected =  [
-                { a: "MA", b: 10, c: "Boston", d: "MA" },
-                { a: "MA", b: 50, c: "Boston", d: "MA" },
-                { a: "NY", b: 20, c: "Albany", d: "NY" }
-            ];
+			const expected =  [
+				{ a: "MA", b: 10, c: "Boston", d: "MA" },
+				{ a: "MA", b: 50, c: "Boston", d: "MA" },
+				{ a: "NY", b: 20, c: "Albany", d: "NY" }
+			];
 
-            assert.deepEqual(actual, expected);
-        });
+			assert.deepEqual(actual, expected);
+		});
 
-        it("excludes unmatched rows from the left and right lists", () => {
-            const actual = [{a: "MA", b: 10}, {a: "WA", b: 50}, {a: "NY", b: 20}]
-                .innerJoin([{a: "MA", c: "Boston"}, {a: "NY", c: "Albany"}, {d: "CA", c: "Sacramento"}], ["a"], ["a"]);
+		it("excludes unmatched rows from the left and right lists", () => {
+			const actual = [{a: "MA", b: 10}, {a: "WA", b: 50}, {a: "NY", b: 20}]
+				.innerJoin([{a: "MA", c: "Boston"}, {a: "NY", c: "Albany"}, {d: "CA", c: "Sacramento"}], ["a"], ["a"]);
 
-            const expected =  [
-                { a: "MA", b: 10, c: "Boston" },
-                { a: "NY", b: 20, c: "Albany" }
-            ];
+			const expected =  [
+				{ a: "MA", b: 10, c: "Boston" },
+				{ a: "NY", b: 20, c: "Albany" }
+			];
 
-            assert.deepEqual(actual, expected);
-        });
-    });
+			assert.deepEqual(actual, expected);
+		});
+	});
 
 });
--- js/test/csv-io.test.js
+++ js/test/csv-io.test.js
@@ -2,75 +2,75 @@ const assert = require("assert");
 const {parseCsLine} = require("../csv-io.js");
 
 describe("Csv IO module", function() {
-    describe("#parseCsLine()", function() {
-        it("parses an empty line as one empty value", function() {
-            assert.deepEqual(parseCsLine(""), [""]);
-        });
+	describe("#parseCsLine()", function() {
+		it("parses an empty line as one empty value", function() {
+			assert.deepEqual(parseCsLine(""), [""]);
+		});
 
-        it("parses one value as one value", function() {
-            assert.deepEqual(parseCsLine("aaa"), ["aaa"]);
-        });
+		it("parses one value as one value", function() {
+			assert.deepEqual(parseCsLine("aaa"), ["aaa"]);
+		});
 
-        it("parses comma-separated values as many values", function() {
-            assert.deepEqual(parseCsLine("aaa,bbb,ccc"), ["aaa", "bbb", "ccc"]);
-        });
+		it("parses comma-separated values as many values", function() {
+			assert.deepEqual(parseCsLine("aaa,bbb,ccc"), ["aaa", "bbb", "ccc"]);
+		});
 
-        it("parses a trailing comma as one empty string value", function() {
-            assert.deepEqual(parseCsLine("aaa,bbb,ccc,"), ["aaa", "bbb", "ccc", ""]);
-        });
+		it("parses a trailing comma as one empty string value", function() {
+			assert.deepEqual(parseCsLine("aaa,bbb,ccc,"), ["aaa", "bbb", "ccc", ""]);
+		});
 
-        it("parses double-quoted values", function() {
-            assert.deepEqual(parseCsLine(`"aaa","bbb","ccc"`), ["aaa", "bbb", "ccc"]);
-        });
+		it("parses double-quoted values", function() {
+			assert.deepEqual(parseCsLine(`"aaa","bbb","ccc"`), ["aaa", "bbb", "ccc"]);
+		});
 
-        it("parses two double quotes as an escaped double quote", function() {
-            assert.deepEqual(parseCsLine(`"aaa","b""bb","ccc"`), ["aaa", `b"bb`, "ccc"]);
-        });
+		it("parses two double quotes as an escaped double quote", function() {
+			assert.deepEqual(parseCsLine(`"aaa","b""bb","ccc"`), ["aaa", `b"bb`, "ccc"]);
+		});
 
-        it("throws on mismatched a double quote", function() {
-            assert.throws(() => parseCsLine(`"aaa","b"bb","ccc"`), {
-                name: "Error",
-                message: `Mismatched double quote in line <"aaa","b"bb","ccc">`
-            });
-        });
+		it("throws on mismatched a double quote", function() {
+			assert.throws(() => parseCsLine(`"aaa","b"bb","ccc"`), {
+				name: "Error",
+				message: `Mismatched double quote in line <"aaa","b"bb","ccc">`
+			});
+		});
 
-        it("throws on a double quote in a non-double-quoted value", function() {
-            assert.throws(() => parseCsLine(`b"bb`), {
-                name: "Error",
-                message: `Values that contain double quotes must be double-quoted, see line <b"bb>`
-            });
-        });
+		it("throws on a double quote in a non-double-quoted value", function() {
+			assert.throws(() => parseCsLine(`b"bb`), {
+				name: "Error",
+				message: `Values that contain double quotes must be double-quoted, see line <b"bb>`
+			});
+		});
 
-        it("parses an escaped comma", function() {
-            assert.deepEqual(parseCsLine(`"aaa","b,bb","ccc"`), ["aaa", "b,bb", "ccc"]);
-        });
+		it("parses an escaped comma", function() {
+			assert.deepEqual(parseCsLine(`"aaa","b,bb","ccc"`), ["aaa", "b,bb", "ccc"]);
+		});
 
-        it("throws on unmatched closing quotes", function() {
-            assert.throws(() => parseCsLine(`"aaa",bb"`), {
-                name: "Error",
-                message: `Values that contain double quotes must be double-quoted, see line <"aaa",bb">`
-            });
-        });
+		it("throws on unmatched closing quotes", function() {
+			assert.throws(() => parseCsLine(`"aaa",bb"`), {
+				name: "Error",
+				message: `Values that contain double quotes must be double-quoted, see line <"aaa",bb">`
+			});
+		});
 
-        it("throws on unmatched middle quotes", function() {
-            assert.throws(() => parseCsLine(`"aaa",bb"b`), {
-                name: "Error",
-                message: `Values that contain double quotes must be double-quoted, see line <"aaa",bb"b>`
-            });
-        });
+		it("throws on unmatched middle quotes", function() {
+			assert.throws(() => parseCsLine(`"aaa",bb"b`), {
+				name: "Error",
+				message: `Values that contain double quotes must be double-quoted, see line <"aaa",bb"b>`
+			});
+		});
 
-        it("throws on unmatched opening quotes", function() {
-            assert.throws(() => parseCsLine(`"aaa","bbb`), {
-                name: "Error",
-                message: `Double quote not closed in line <"aaa","bbb>`
-            });
-        });
+		it("throws on unmatched opening quotes", function() {
+			assert.throws(() => parseCsLine(`"aaa","bbb`), {
+				name: "Error",
+				message: `Double quote not closed in line <"aaa","bbb>`
+			});
+		});
 
-        it("throws on single double quote as a value", function() {
-            assert.throws(() => parseCsLine(`"aaa",",bbb`), {
-                name: "Error",
-                message: `Double quote not closed in line <"aaa",",bbb>`
-            });
-        });
-    });
+		it("throws on single double quote as a value", function() {
+			assert.throws(() => parseCsLine(`"aaa",",bbb`), {
+				name: "Error",
+				message: `Double quote not closed in line <"aaa",",bbb>`
+			});
+		});
+	});
 });
