9b3113772b7a8de9446825a4d92ccc28e4b79918
Prakhar Sahay <prakhar.studios@gmail.com>
2021-12-12 22:07:57 -0500

    [Test] Add Logarithmic Window Unit Test

--- js/test/moving-average.test.js
+++ js/test/moving-average.test.js
@@ -52,5 +52,34 @@ describe("Moving Average module", () => {
 
 			assert.deepEqual(actual, expected);
 		});
+
+
+		it("aggregates logarithmic windows", () => {
+			const input = [{a: 1, b: 20}, {a: 2, b: 21}, {a: 4, b: 19}, {a: 8, b: 20}, {a: 16, b: 10}];
+			const actual = movingAverage({data: input, x: "a", y: "b", windowWidth: 1.5, stepWidth: 1, logarithmic: true});
+
+			const expected =  [
+				{
+					a: 1.75,
+					a_window: [1, 2.5],
+					b: 20.5,
+					n: 2
+				},
+				{
+					a: 3.5,
+					a_window: [2, 5],
+					b: 20,
+					n: 2
+				},
+				{
+					a: 7,
+					a_window: [4, 10],
+					b: 19.5,
+					n: 2
+				}
+			];
+
+			assert.deepEqual(actual, expected);
+		});
 	});
 });
